286d6435d35e2aa144fc92941023e733b457f7d2
Isacre <isaacalvesx7@gmail.com>
2023-03-02 22:55:58 -0300

    responsiveness

--- src/components/carrousel/index.tsx
+++ src/components/carrousel/index.tsx
@@ -15,7 +15,7 @@ function Carousel() {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 15000,
     beforeChange: () => {
       setRefresh(!Refresh);
